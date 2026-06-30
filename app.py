4cfb06207b134385f74a44180fc8aa81474a89db
clxx1044-max <clxx1044@gmail.com>
2026-06-30 10:31:45 -0400

    Update app.py

--- app.py
+++ app.py
@@ -549,7 +549,7 @@ with t_reg:
     st.markdown('<h2 class="tab-h">Create your account</h2>', unsafe_allow_html=True)
     st.markdown(
         '<p class="tab-p">Register with your name and email before writing a review.' 
-        'Although your information will be hashed, we will be able to connect your name with your responses</p>',
+        '\n Although your information will be hashed, we will be able to connect your name with your responses</p>',
         unsafe_allow_html=True,
     )
 
