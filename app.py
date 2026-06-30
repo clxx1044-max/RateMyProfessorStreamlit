6178fa7bd3d6c5206a8d92e5369b25bb00d51549
clxx1044-max <clxx1044@gmail.com>
2026-06-30 10:31:17 -0400

    Update app.py

--- app.py
+++ app.py
@@ -548,7 +548,8 @@ t_reg, t_see, t_write = st.tabs(["  Register  ", "  See Reviews  ", "  Write a R
 with t_reg:
     st.markdown('<h2 class="tab-h">Create your account</h2>', unsafe_allow_html=True)
     st.markdown(
-        '<p class="tab-p">Register with your name and email before writing a review. </p>',
+        '<p class="tab-p">Register with your name and email before writing a review.' 
+        'Although your information will be hashed, we will be able to connect your name with your responses</p>',
         unsafe_allow_html=True,
     )
 
